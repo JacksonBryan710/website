b424fb1ffe63bd4478b3384dad3422e0af2e6b3d
jacksonbryan710 <jacksonbryan710@gmail.com>
2026-02-16 23:01:16 -0500

    added CNAME file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
+import { resolve } from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: resolve(__dirname, 'public/CNAME'),
+                    dest: '.',
+                },
+            ],
+        }),
+    ],
+});
